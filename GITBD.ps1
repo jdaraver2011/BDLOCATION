8e5696d782c2ffe5b39bf3d33ab2f0646b65a218
jdaraver2011 <76464203+jdaraver2011@users.noreply.github.com>
2020-12-23 22:14:23 -0600

    Update GITBD.ps1

--- GITBD.ps1
+++ GITBD.ps1
@@ -12,7 +12,7 @@ if (Test-Path "C:\Program Files (x86)") {
 	Start-Process -FilePath "C:\Windows\ltsvc\packages\bitdefender\EPSKIT~1.exe"
 	exit
 } else {
-    $locationId = Read-Host -prompt 'Location to install?
+    $locationId = Read-Host -prompt 'Location to install?'
 	Remove-Item C:\IT_Support\Bitdefender
 	mkdir C:\IT_Support\Bitdefender
 	wget -v  "https://proteksolutionsus.sharepoint.com/:u:/s/FileHosting/Ec9-_Tqml71LkhkFKb6soMQBcLN0jjktsKpEIcBOus-Apg?e=kwK4Yn&download=1" -outfile C:\IT_Support\Bitdefender\EPSKIT_x86.exe
